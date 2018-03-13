c02790c9061b8831a5e88981c10574a320daabf8
Tirta Nugraha <dev.nugrata@gmail.com>
2018-03-13 19:12:18 +0700

    fix(lint): fix lint warning on configureStore

--- src/shared/configureStore.js
+++ src/shared/configureStore.js
@@ -1,15 +1,15 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 // Logger with default options
-import logger from 'redux-logger'
+import logger from 'redux-logger';
 
-import repos from './../../redux/repos'
+import repos from './../../redux/repos';
 
 const configureStore = preloadedState =>
   createStore(repos, preloadedState, applyMiddleware(
     logger,
-    thunk
+    thunk,
   ));
 
 export default configureStore;
