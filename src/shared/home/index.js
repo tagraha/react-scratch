1077a605b7417db0b4643f343b5d6152d4cab2b3
Tirta Nugraha <dev.nugrata@gmail.com>
2018-03-19 10:56:54 +0700

    other(view): change the home page intro

--- src/shared/home/index.js
+++ src/shared/home/index.js
@@ -7,35 +7,31 @@ import './index.css';
 class Home extends Component {
   render() {
     return (
-      <div className="row">
+      <div className="container">
         <Helmet>
           <title>Welcome to our Homepage</title>
           <meta name="description" content="react-scratch" />
         </Helmet>
-        <div className="column">
-          <p className="selected">About</p>
-          <p>
-            <Link to="/repo">tagraha repo (async demo)</Link>
-          </p>
+
+        <div className="row">
+          <div className="column">
+            <ul>
+              <li>
+                <Link to="/repo">tagraha repo (async demo)</Link>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="column">
-          <blockquote>
-            <p><em>Sometimes when I close my eyes, I cant see :</em></p>
-          </blockquote>
-          <p>
-            Twice a year we invest a small amount of mana in a large number of wizards (recently
-            105).
-          </p>
-          <p>
-            The wizards move to the Highlands of Scotland for 3 months, during which we work
-            intensively with them to get the magic into the best possible shape and refine their
-            pitch to patrons. Each cycle culminates in Demo Day, when the wizards present their
-            magic to a carefully selected, invite-only audience.
-          </p>
-          <p>
-            But WC doesn’t end on Demo Day. We and the WC alumni network continue to help mages for
-            the life of their magic, and beyond.
-          </p>
+
+        <div className="row">
+          <div className="column">
+            <blockquote>
+              <p><em>Motivation</em></p>
+            </blockquote>
+            <p>
+              There is a lot react universal boilerplate out there. I just want to make it as simple as possible with minimal setup
+            </p>
+          </div>
         </div>
       </div>
     );
