84c79bf9c71e5d9e0915b0735624279d036b4ebf
Tirta Nugraha <dev.nugrata@gmail.com>
2018-03-22 14:00:53 +0700

    other(eslint): fix eslint warning

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from "react";
-import { Switch, Route } from "react-router-dom";
-import routes from "./routes";
-import "./App.css";
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import routes from './routes';
+import random from './../../utils/randomstring';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <Switch>
-          {routes.map((route, i) => <Route key={i} {...route} />)}
+          {routes.map((route) => <Route key={random(7)} {...route} />)}
         </Switch>
       </div>
     );
